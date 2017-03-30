62078b9ed7a72e18ddff9cfd97fdf57372c3cbe6
Adam Gorbahn <adamgorbahn@hotmail.com>
2017-03-30 13:48:09 -0700

    make populate grids

--- js/prefabs/Board.js
+++ js/prefabs/Board.js
@@ -10,11 +10,10 @@ Match3.Board = function(state, rows, cols, blockVariations) {
   //main grid
   this.grid = [];
 
-  var i,j;
-  for(i = 0; i < rows; i++) {
+  for(var i = 0; i < rows; i++) {
     this.grid.push([]);
 
-    for(j = 0; j < cols; j++) {
+    for(var j = 0; j < cols; j++) {
       this.grid[i].push(0);
     }
   }
@@ -24,13 +23,64 @@ Match3.Board = function(state, rows, cols, blockVariations) {
 
   this.RESERVE_ROW = 5;
 
-  for(i = 0; i < this.RESERVE_ROW; i++) {
+  for(var i = 0; i < this.RESERVE_ROW; i++) {
     this.reserveGrid.push([]);
 
-    for(j = 0; j < cols; j++) {
+    for(var j = 0; j < cols; j++) {
       this.reserveGrid[i].push(0);
     }
   }
 
+  this.populateGrids();
+  this.populateReserveGrid();
+
+  this.consoleLog();
+
 };
 
+
+Match3.Board.prototype.populateGrids = function() {
+  var Variations;
+
+  for(var i = 0; i < this.rows; i++) {
+    for(var j = 0; j < this.cols; j++) {
+      Variations = Math.floor(Math.random() * this.blockVariations) + 1;
+      this.grid[i][j] = Variations;
+    }
+  }
+};
+
+Match3.Board.prototype.populateReserveGrid = function() {
+  var Variations;
+
+  for(var i = 0; i < this.RESERVE_ROW; i++) {
+    for(var j = 0; j < this.cols; j++) {
+      Variations = Math.floor(Math.random() * this.blockVariations) + 1;
+      this.reserveGrid[i][j] = Variations;
+    }
+  }
+};
+
+Match3.Board.prototype.consoleLog =  function() {
+  
+  var stringOuput = '';
+
+  for(var i = 0; i < this.RESERVE_ROW; i++) {
+    stringOuput += '\n';
+    for(var j = 0; j < this.cols; j++) {
+      stringOuput += ' ' + this.reserveGrid[i][j];
+    }
+  }
+
+  stringOuput += '\n';
+  stringOuput += '\n';
+
+  for(var i = 0; i < this.rows; i++) {
+     stringOuput += '\n';
+    for(var j = 0; j < this.cols; j++) {
+      stringOuput += ' ' + this.grid[i][j];
+    }
+  }
+
+  console.log(stringOuput);
+};
